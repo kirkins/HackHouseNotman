cb1e8f8da0627147a148ee914387abab1d890027
philip kirkbride <kirkins@gmail.com>
2016-11-12 20:13:46 +0000

    connect camera to port 8888

--- pi/camera.py
+++ pi/camera.py
@@ -1,9 +1,42 @@
+'''
+    Simple socket server using threads
+'''
+ 
+import socket
+import sys
 import time
 import picamera
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('img.jpg')
+ 
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    #take photo
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture('img.jpg')
+     
+s.close()
